ad15760f07449cd37f38d12fcde54315efb01eae
Your Name <just.misterr@gmail.com>
2024-04-28 19:38:32 +0200

    Character info

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -9,7 +9,8 @@ import mjolnir from '../../resources/img/mjolnir.png';
 class RandomChar extends Component {
     state = {
         char: {},
-        loading: true 
+        loading: true,
+        error: false 
     }
 
     marvelService = new MarvelService();
@@ -31,6 +32,12 @@ class RandomChar extends Component {
         })
     }
 
+    onCharLoading = () => {
+        this.setState({
+            loading: true
+        })
+    }
+
     onError = () => {
         this.setState({
             loading: false,
@@ -40,6 +47,7 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
